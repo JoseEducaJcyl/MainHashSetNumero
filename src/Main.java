d949debfe4512193c1ab5c1c36a222fcdd7a9ff2
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-21 12:35:15 +0100

    Ejercicio sobre HashSet y su funcionamiento

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
+    public static void main(String[] args) {
+        //Creamos un set de numeros
+        Set<Integer> numeros = new HashSet<>();
+        //Le añadimos algunos numeros, intentamos añadir duplicados
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(4);
+
+        //Recorremos el set y lo mostramos por pantalla, y se puede observar que no admite duplicados
+        for (Integer numero : numeros) {
+            System.out.println(numero);
+        }
+    }
 }
